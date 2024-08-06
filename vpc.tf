26694c5788bd7c047e24dd354af18a84f010dabb
VenkateshAvy <venkateshavy123@gmail.com>
2024-08-06 15:05:42 +0530

    created subnets for ibm vpc

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,27 @@ resource "aws_vpc" "ibm-vpc" {
     Name = "ibm-vpc"
   }
 }
+resource "aws_subnet" "ibm-web-subnet" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "ibm-web-subnet"
+  }
+}
+resource "aws_subnet" "ibm-db-subnet" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "ibm-db-subnet"
+  }
+}
+resource "aws_subnet" "ibm-api-subnet" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "ibm-api-subnet"
+  }
+}
